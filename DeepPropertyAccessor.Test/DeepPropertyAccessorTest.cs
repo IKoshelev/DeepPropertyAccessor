f166908649f3b938d0775567c0db4888c0921b95
IKoshelev <koshelew@live.com>
2017-02-20 21:32:14 +0100

    Initial commit (coding mostly done)

---
+++ DeepPropertyAccessor.Test/DeepPropertyAccessorTest.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeepPropertyAccessor.Test
+{
+    [TestFixture]
+    public class DeepPropertyAccessorTest
+    {
+        [Test]
+        public void ItCanGetDeepFieldsOfValueType()
+        {
+            var subject = new Subject<Subject<Subject<Subject<int>>>>();
+
+            var val = subject.DeepGetStruct(x => x.Prop.Field.Prop.Field);
+
+            Assert.That(val.GetType() == typeof(int));
+            Assert.That(val == 0);
+        }
+
+        [Test]
+        public void ItCanGetDeepPropertiesOfValueType()
+        {
+            var subject = new Subject<Subject<Subject<Subject<int>>>>();
+
+            var val = subject.DeepGetStruct(x => x.Prop.Field.Field.Prop);
+
+            Assert.That(val.GetType() == typeof(int));
+            Assert.That(val == 0);
+        }
+
+        [Test]
+        public void ItCanGetDeepFieldsOfReferenceType()
+        {
+            var subject = new Subject<Subject<Subject<Subject<RefTest>>>>();
+
+            var val = subject.DeepGet(x => x.Prop.Field.Prop.Field);
+
+            Assert.That(val.GetType() == typeof(RefTest));
+            Assert.That(val != null);
+        }
+
+        [Test]
+        public void ItCanGetDeepPropertiesOfReferenceType()
+        {
+            var subject = new Subject<Subject<Subject<Subject<RefTest>>>>();
+
+            var val = subject.DeepGet(x => x.Prop.Field.Prop.Prop);
+
+            Assert.That(val.GetType() == typeof(RefTest));
+            Assert.That(val != null);
+        }
+
+        [Test]
+        public void DeepGetWillReturnNullAndCallOnNullInChainDelegate()
+        {
+            var subject = new Subject<Subject<Subject<Subject<RefTest>>>>();
+
+            subject.Prop.Field = null;
+
+            string fullChain = "";
+            List<AccessorChainPart> chainUpToNull = null;
+
+            var val = subject
+                        .DeepGet(x => x.Prop.Field.Field.Prop,
+                            (_chainUpToNull, _fullChain) => {
+                                chainUpToNull = _chainUpToNull;
+                                fullChain = _fullChain.ToChainDescription();
+                            });
+
+            Assert.That(val == null);
+            Assert.That(chainUpToNull != null);
+            Assert.That(chainUpToNull.Count == 3);
+            Assert.That(chainUpToNull.ElementAt(0).Name == "(root)Subject<Subject<Subject<Subject<RefTest>>>>");
+            Assert.That(chainUpToNull.ElementAt(0).Value == subject);
+            Assert.That(chainUpToNull.ElementAt(1).Name == "Prop");
+            Assert.That(chainUpToNull.ElementAt(1).Value == subject.Prop);
+            Assert.That(chainUpToNull.ElementAt(2).Name == "Field");
+            Assert.That(chainUpToNull.ElementAt(2).Value == null);
+            Assert.That(fullChain == "(root).Prop.Field.Field.Prop");
+        }
+
+        [Test]
+        public void DeepGetStructWillReturnNullAndCallOnNullInChainDelegate()
+        {
+            var subject = new Subject<Subject<Subject<Subject<int>>>>();
+
+            subject.Prop.Field = null;
+
+            string fullChain = "";
+            List<AccessorChainPart> chainUpToNull = null;
+
+            var val = subject
+                        .DeepGetStruct(x => x.Prop.Field.Field.Prop,
+                            (_chainUpToNull, _fullChain) => {
+                                chainUpToNull = _chainUpToNull;
+                                fullChain = _fullChain.ToChainDescription();
+                            });
+
+            Assert.That(val == null);
+            Assert.That(chainUpToNull != null);
+            Assert.That(chainUpToNull.Count == 3);
+            Assert.That(chainUpToNull.ElementAt(0).Name == "(root)Subject<Subject<Subject<Subject<Int32>>>>");
+            Assert.That(chainUpToNull.ElementAt(0).Value == subject);
+            Assert.That(chainUpToNull.ElementAt(1).Name == "Prop");
+            Assert.That(chainUpToNull.ElementAt(1).Value == subject.Prop);
+            Assert.That(chainUpToNull.ElementAt(2).Name == "Field");
+            Assert.That(chainUpToNull.ElementAt(2).Value == null);
+            Assert.That(fullChain == "(root).Prop.Field.Field.Prop");
+        }
+
+
+        [Test]
+        public void TestMethod1()
+        {
+            var subject = new Subject<Subject<int>>();
+
+            var val = subject.DeepGetStruct(x => x.Prop.Field);
+
+            subject.Prop = null;
+
+            var val2 = subject.DeepGetStruct(x => x.Prop.Field, 
+                (chainUpToNull, fullChain) =>
+            {
+                var chain = fullChain.ToChainDescription();
+            });
+
+        }
+    }
+
+    public class Subject<T> where T: new()
+    {
+        public T Prop { get; set; }
+        public T Field { get; set; }
+
+        public Subject ()
+        {
+            Prop = new T();
+            Field = new T();
+        }
+    }
+
+    public class RefTest
+    {
+
+    }
+}
---
+++ DeepPropertyAccessor/DeepPropertyAccessor.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepPropertyAccessor
+{
+    public static class DeepPropertyAccessor
+    {
+        public static TProp DeepGet<TSource,TProp>(
+            this TSource source, 
+            Expression<Func<TSource, TProp>> getter, 
+            Action<List<AccessorChainPart>, Expression<Func<TSource, TProp>>> onNullInChain = null)
+            where TProp : class
+        {
+            var chain = new List<AccessorChainPart>();
+
+            foreach(var chainPart in ParseAccessChain(source, getter))
+            {
+                chain.Add(chainPart);
+                if(chainPart.Value == null)
+                {
+                    onNullInChain(chain, getter);
+                    return null;
+                }
+            }
+
+            return (TProp)chain.Last().Value;
+        }
+
+        public static TProp? DeepGetStruct<TSource, TProp>(
+            this TSource source,
+            Expression<Func<TSource, TProp>> getter,
+            Action<List<AccessorChainPart>, Expression<Func<TSource, TProp>>> onNullInChain = null)
+            where TProp : struct
+        {
+            var chain = new List<AccessorChainPart>();
+
+            foreach (var chainPart in ParseAccessChain(source, getter))
+            {
+                chain.Add(chainPart);
+                if (chainPart.Value == null)
+                {
+                    onNullInChain(chain, getter);
+                    return null;
+                }
+            }
+
+            return (TProp?)chain.Last().Value;
+        }
+
+        public static string ToChainDescription(this Expression expr)
+        {
+            var str = expr.ToString();
+            var firstDotIndex = str.IndexOf('.');
+            if(firstDotIndex == -1)
+            {
+                throw new ArgumentException($"{str} is not a chain");
+            }
+
+            return "(root)" + str.Substring(firstDotIndex);
+        }
+
+        private static IEnumerable<AccessorChainPart> ParseAccessChain<TSource, TProp>(
+            TSource source, 
+            Expression<Func<TSource, TProp>> getter)
+        {
+            yield return new AccessorChainPart(source, typeof(TSource));
+
+            List<MemberExpression> chainOfMemberAccess = new List<MemberExpression>();
+
+            var current = getter.Body as MemberExpression;
+            while (current != null)
+            {
+                chainOfMemberAccess.Add(current);
+                current = current.Expression as MemberExpression;
+            }
+
+            chainOfMemberAccess.Reverse();
+
+            object lastValue = source;
+            if (chainOfMemberAccess.Any())
+            {
+                var firstAccess = chainOfMemberAccess.ElementAt(0);
+                lastValue = GetValueFromMemberExpression(source, firstAccess);
+
+                yield return new AccessorChainPart(lastValue, firstAccess.Member);
+            }
+
+            for(int count1 = 1; count1 < chainOfMemberAccess.Count(); count1++)
+            {
+                var currentAccess = chainOfMemberAccess.ElementAt(count1);
+                lastValue = GetValueFromMemberExpression(lastValue, currentAccess);
+                yield return new AccessorChainPart(lastValue, currentAccess.Member);
+            }
+        }
+
+        private static object GetValueFromMemberExpression(object source, MemberExpression expr)
+        {
+            var propInfo = (expr.Member as PropertyInfo);
+            if (propInfo != null)
+            {
+                return propInfo.GetValue(source);
+
+            }
+            var fieldInfo = (expr.Member as FieldInfo);
+            if (fieldInfo != null)
+            {
+                return fieldInfo.GetValue(source);
+            }
+
+            throw new ArgumentException($"{expr.ToString()} is neither Field nor Property access.");
+
+        }
+    }
+
+    public class AccessorChainPart
+    {
+        public object Value { get; private set; }
+        public string Name { get; private set; }
+        public MemberInfo MemberInfo { get; private set; }
+
+        public AccessorChainPart()
+        {
+        }
+
+        public AccessorChainPart(object value, MemberInfo memberInfo)
+        {
+            Value = value;
+            MemberInfo = memberInfo;
+            Name = MemberInfo.Name;
+        }
+
+        public AccessorChainPart(object value, Type root)
+        {
+            Value = value;
+            Name = "(root)" + GetClassNameAccountingForGeneric(root);
+        }
+
+
+        private string GetClassNameAccountingForGeneric(Type targetType)
+        {
+            if (targetType.IsGenericType == false)
+            {         
+                return targetType.Name;
+            }
+
+            if (targetType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                return targetType.GetGenericArguments().Select(GetClassNameAccountingForGeneric).Single() + "?";
+            }
+
+            var baseType = targetType.GetGenericTypeDefinition();
+            var name = baseType.Name.Split('`')[0];
+            var paramNames = targetType.GetGenericArguments().Select(GetClassNameAccountingForGeneric);
+            var constructorName = name + "<" + string.Join(",", paramNames) + ">";
+            return constructorName;
+
+        }
+
+    }
+}
